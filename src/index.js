af037ff85dae1d8372466cb52ae75cb689a451f7
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-23 20:21:23 +0300

    feat: add project structure

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const ToDoList = () => {
-    const items = ['Drink Coffee', 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-}
+import ToDoList from './components/todo-list';
+import AppHeader from './components/app-header';
+import SearchPannel from './components/search-panal';
 
-const AppHeader = () => {
-    return <h1>My Todo list</h1>;
-}
-
-const SearchPannel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px'
-    }
-    return <input
-        style={searchStyle}
-        placeholder={searchText} />;
-}
 
 const App = () => {
 
