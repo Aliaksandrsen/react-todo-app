f2521cd14f6b76557cf0f0686c9d5b39614bc3f6
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-23 16:51:58 +0300

    feat: add basic components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ToDoList = () => {
+    const items = ['Drink Coffee', 'Build Awesome App'];
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+}
+
+const AppHeader = () => {
+    return <h1>My Todo list</h1>;
+}
+
+const SearchPannel = () => {
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    }
+    return <input
+        style={searchStyle}
+        placeholder={searchText} />;
+}
+
+const App = () => {
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPannel />
+            <ToDoList />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
