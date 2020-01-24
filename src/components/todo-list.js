35e9e38e5ddd058a5698d55ce5b69ccd441c85cd
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-24 16:27:31 +0300

    feat: add arrs as components props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ToDoListItem from './todo-list-item';
 
-const ToDoList = () => {
+const ToDoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+        return (
+            <li><ToDoListItem
+                label={item.label}
+                important={item.important} />
+            </li>
+        );
+    });
+
     return (
         <ul>
-            <li><ToDoListItem/></li>
-            <li><ToDoListItem/></li>
+           {elements}
         </ul>
     );
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ import SearchPannel from './components/search-panal';
 
 const App = () => {
 
+    const todoData = [
+        {label: 'Drink Coffee', important: false},
+        {label: 'Make Awesome App', important: true},
+        {label: 'Have a lunch', important: false},
+    ]
+
     return (
         <div>
             <AppHeader />
             <SearchPannel />
-            <ToDoList />
+            <ToDoList todos={todoData}/>
         </div>
     );
 }
