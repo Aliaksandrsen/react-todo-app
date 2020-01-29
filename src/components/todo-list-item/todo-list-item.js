109f334051f967ac6eb5fec8a8c145a0d012a1fa
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-29 18:55:45 +0300

    feat: add custom events

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,29 +4,58 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  render() {
-    const { label, important = false } = this.props;
+  state = {
+    done: false,
+    important: false,
+  };
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  onLabelClick = () => {
+    this.setState((state) => {
+      return {
+        done: !state.done,
+      };
+    });
+
+  };
+
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important,
+      };
+    });
+  };
+
+  render() {
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
-      <span className="todo-list-item" >
+      <span className={classNames} >
         <span
           className="todo-list-item-label"
-          style={style}>
+          onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button type="button"
-          className="btn btn-outline-success btn-sm float-right">
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-          className="btn btn-outline-danger btn-sm float-right">
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
